d1e9047f819774e11559f0a431cc77fd8779fd44
uvinnobusu <uvinnobusu@gmail.com>
2023-08-09 22:24:29 +0300

    update files

---
+++ src/components/header/HeaderPullMenu.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class HeaderPullMenu extends React.Component<{}, {}> {
+    constructor(props: {}) {
+        super(props);
+
+        this.state = {}
+    }
+
+    render(): JSX.Element {
+        return (
+            <div className="pullmenu"></div>
+        )
+    }
+}
+
+export default HeaderPullMenu;
