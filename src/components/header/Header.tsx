c628e1383e9915d0bad298aa7278ee69086384e7
uvinnobusu <uvinnobusu@gmail.com>
2023-08-10 00:19:31 +0300

    add pull menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import HeaderPullMenu from "./HeaderPullMenu";
 import ispringLogo from "./img/ispring-institute-logo.svg";
 
+type TypeState = {
+    showPullMenu: boolean,
+}
 
-class Header extends React.Component<{}, {}> {
+class Header extends React.Component<{}, TypeState> {
     constructor(props: {}) {
         super(props);
 
-        this.state = {}
+        this.state = {
+            showPullMenu: false,
+        }
+
+        // this.pullMenuVisibility = this.pullMenuVisibility.bind(this);
     }
 
     render(): JSX.Element {
@@ -18,13 +26,20 @@ class Header extends React.Component<{}, {}> {
                             <img src={ispringLogo} alt="institute ispring"/>
                         </div>
                         <ul className="header__navigation">
-                            <li>
+                            <li onClick={(): void => {
+                                this.setState({
+                                    showPullMenu: !this.state.showPullMenu,
+                                })
+                            }}>
                                 <a className="header__navigation-link pullmenu" href="#!">
                                     Абитуриентам
                                     <svg width="11" height="7" viewBox="0 0 13 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L6.5 6L12 1" stroke="#FF603D" strokeWidth="1.5"/></svg>
                                 </a>
+                                {(this.state.showPullMenu) ? <HeaderPullMenu /> : null}
                             </li>
-                            <li>
+                            <li onMouseOver={(): void => {
+                                console.log(1)
+                            }}>
                                 <a className="header__navigation-link" href="#!">
                                     Мероприятия
                                 </a>
@@ -48,10 +63,12 @@ class Header extends React.Component<{}, {}> {
                             <svg width="28" height="28" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg"><g clipPath="url(#clip0_14_84)"><path d="M14.5834 7.29167C14.5834 6.51812 14.8907 5.77625 15.4376 5.22927C15.9846 4.68229 16.7265 4.375 17.5 4.375C18.2736 4.375 19.0155 4.68229 19.5624 5.22927C20.1094 5.77625 20.4167 6.51812 20.4167 7.29167C22.0915 8.08358 23.5192 9.31631 24.5468 10.8577C25.5744 12.3991 26.1632 14.1911 26.25 16.0417V20.4167C26.3598 21.3233 26.6809 22.1915 27.1875 22.9514C27.6941 23.7113 28.372 24.3416 29.1667 24.7917H5.83337C6.62808 24.3416 7.30601 23.7113 7.81261 22.9514C8.3192 22.1915 8.64029 21.3233 8.75004 20.4167V16.0417C8.8369 14.1911 9.42569 12.3991 10.4533 10.8577C11.4809 9.31631 12.9086 8.08358 14.5834 7.29167Z" stroke="#FF603D" strokeLinecap="round" strokeLinejoin="round"/><path d="M13.125 24.7917V26.25C13.125 27.4103 13.5859 28.5231 14.4064 29.3436C15.2269 30.1641 16.3397 30.625 17.5 30.625C18.6603 30.625 19.7731 30.1641 20.5936 29.3436C21.4141 28.5231 21.875 27.4103 21.875 26.25V24.7917" stroke="#FF603D" strokeLinecap="round" strokeLinejoin="round"/></g><defs><clipPath id="clip0_14_84"><rect width="35" height="35" fill="white"/></clipPath></defs></svg>
                         </div>
                     </div>
+
                 </div>
             </header>
         )
     }
+
 }
 
 export default Header;
--- src/components/header/HeaderPullMenu.tsx
+++ src/components/header/HeaderPullMenu.tsx
@@ -1,15 +1,45 @@
 import React from "react";
 
+
+
 class HeaderPullMenu extends React.Component<{}, {}> {
     constructor(props: {}) {
         super(props);
 
-        this.state = {}
     }
 
     render(): JSX.Element {
         return (
-            <div className="pullmenu"></div>
+            <div className="header__pullmenu">
+                <ul className="header__pullmenu-nav">
+                    <li>
+                        <a href="#!" className="header__pullmenu-link">
+                            Бакалавриат
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#!" className="header__pullmenu-link">
+                            Прямые эфиры
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#!" className="header__pullmenu-link">
+                            Грант на обучение
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#!" className="header__pullmenu-link">
+                            Личный кабинет
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#!" className="header__pullmenu-link">
+                            Прием 2023
+                        </a>
+                    </li>
+                </ul>
+                <div className="header__pullmenu-info"></div>
+            </div>
         )
     }
 }
